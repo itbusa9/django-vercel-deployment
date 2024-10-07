6e144757cc4f13768cec89144f0f8e162eb15f81
itb usa 9 <lalchanbadsa12345@gmail.com>
2024-10-07 11:44:54 -0600

    update

--- sitewsgi/settings.py
+++ sitewsgi/settings.py
@@ -184,14 +184,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATICFILES_DIRS = [
-    BASE_DIR/'static'
-]
 
 
 # Default primary key field type
